e3b4378553872f7adc186101e86350777bb0ec4a
Agnieszka Chmielewska <chmielewska.agnieszka@tlen.pl>
2023-03-19 18:50:31 +0100

    commit

--- src/OdwroconaKolejnoscTablicy.java
+++ src/OdwroconaKolejnoscTablicy.java
@@ -7,9 +7,24 @@ public class OdwroconaKolejnoscTablicy {
         colors[2] = "black";
         colors[3] = "yellow";
         colors[4] = "grey";
-
+//
         for (int i = colors.length; i > 0; i--) {
             System.out.println(colors[i-1]);
+        }
+
+        System.out.println("new -----way-----");
+        System.out.println(colors.length/2);
+
+        for (int i=0; i<(colors.length/2); i++){
+            String temp = colors[i];
+            System.out.println(    colors[i] = colors[colors.length-1-i]);
+            System.out.println(    colors[colors.length-1-i] = temp );
+        }
+
+        System.out.println("new -----way-----");
+        System.out.println(colors.length);
+        for (int i=0; i<colors.length; i++){
+            System.out.println(colors[i]);
         }
     }
 }
